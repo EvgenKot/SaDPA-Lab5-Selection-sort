a56084029b0be2c85fd52753da8ca47aea73d977
Evgeny Kot <ukal.evgen@gmail.com>
2022-04-15 00:41:19 +0500

    v0.10.0
    Переформатированно

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /*
 Ращупкин Евгений КЭ-203
 28 вариант
 Lab5
 Сортировка Прямым выбором
-В одномерном массиве упорядочить нечетные элементы, 
+В одномерном массиве упорядочить нечетные элементы,
 оставив четные на местах.
 */
 
-
-void PrintArray(int *arr, int n){
+void PrintArray(int *arr, int n)
+{
     int i;
-    for (i = 0; i<n; i++){
+    for (i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-
-
-
-void selectionSort(int *arr, int size){
+void selectionSort(int *arr, int size)
+{
     int min, tmp;
-    for (int i = 0; i < size - 1; i++) {
-        min = i; 
-        for (int j = i + 1; j < size; j++){  
-            if (arr[j] < arr[min]) 
+    for (int i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min])
                 min = j;
         }
         tmp = arr[i];
@@ -37,41 +37,47 @@ void selectionSort(int *arr, int size){
     }
 }
 
-
-
-int NuberOfOdd(int *arr, int size){
+int NuberOfOdd(int *arr, int size)
+{
     int j = 0;
-    for (int i = 0; i < size; i++){
-        if (arr[i] % 2 != 0){
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
             j++;
         }
     }
     return j;
 }
 
-
-void OddFromArray(int *arr, int size, int *arr2){
+void OddFromArray(int *arr, int size, int *arr2)
+{
     int j = 0;
-    for (int i = 0; i < size; i++){
-        if (arr[i] % 2 != 0){
-            arr2[j] = arr[i];    
-            j++;      
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            arr2[j] = arr[i];
+            j++;
         }
     }
 }
 
-void ReplacedOdd(int *arr, int size, int *arr2){
+void ReplacedOdd(int *arr, int size, int *arr2)
+{
     int j = 0;
-    for (int i = 0; i < size; i++){
-        if (arr[i] % 2 != 0){
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
             arr[i] = arr2[j];
-            j++;      
+            j++;
         }
     }
 }
 
-
-int main() {
+int main()
+{
     int *arr;
     int *arr2;
     int n;
@@ -81,29 +87,30 @@ int main() {
     printf("Len of arr: ");
     scanf("%d", &n);
 
-    arr = (int*)malloc(n * sizeof(int));
-    for (i = 0; i<n; i++){
+    arr = (int *)malloc(n * sizeof(int));
+    for (i = 0; i < n; i++)
+    {
         printf("a[%d] = ", i);
         scanf("%d", &arr[i]);
     }
 
-    PrintArray (arr, n);
+    PrintArray(arr, n);
 
     n2 = NuberOfOdd(arr, n);
-    arr2 = (int*)malloc(n2 * sizeof(int));
+    arr2 = (int *)malloc(n2 * sizeof(int));
     printf("arr2 len = %d\n", n2);
     printf("Odd:\n");
     OddFromArray(arr, n, arr2);
-    PrintArray (arr2, n2);
+    PrintArray(arr2, n2);
 
     selectionSort(arr2, n2);
     printf("Sorted Odd:\n");
-    PrintArray (arr2, n2);
+    PrintArray(arr2, n2);
 
-    ReplacedOdd (arr, n, arr2);
+    ReplacedOdd(arr, n, arr2);
     printf("Replacement:\n");
-    PrintArray (arr, n);
-    
+    PrintArray(arr, n);
+
     free(arr);
     free(arr2);
     return 0;
